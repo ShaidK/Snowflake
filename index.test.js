031275b0da15788f43ce5272203f0ce837b3251a
Shaid Khan <100602151+ShaidK@users.noreply.github.com>
2022-08-17 15:10:00 +0100

    V1.1.0
    Update Commit - Committing integration of the testing the Function for extracting properties from the Extension Manifest
    
    - Integrated the 'jest' libraries to unit test the extracting properties from Extension Manifest
    - Integrated this within the 'build' script pipeline

--- index.test.js
+++ index.test.js
@@ -1,14 +1,14 @@
 /*
- * File: index.js | Note: Integrated main entry point for the Snowflake Project
+ * File: index.test.js | Note: Maintain testing of the component within the Snowflake Action
  */
 
-/*
+/* 
  * MIT License
  * 
  * Copyright (c) 2022 Shaid Khan
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ * of this software and associated documentation files (the 'Software'), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
@@ -17,7 +17,7 @@
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -26,10 +26,8 @@
  * SOFTWARE.
  */
 
-function getPackageProperty(property) {
-    return property;
-}
+var function_obj = require('./index.js')
 
-module.exports = {
-    getPackageProperty: getPackageProperty
-};
+test('Note: Maintain testing to successfully obtain the Property "version" within the Extension Manifest', () => {
+    expect(function_obj.getPackageProperty('version')).toMatch(/^(\d+).(\d+).(\d+)$/)
+});
